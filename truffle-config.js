68cdb9a5fad24d0cae37c3e6e4b2a084880e9b2b
Anatolii Kucheruk <akucheruk@vareger.com>
2018-01-29 20:26:37 +0200

    Changed truffle config to use secrets file.

--- truffle-config.js
+++ truffle-config.js
@@ -1,27 +1,36 @@
-var HDWalletProvider = require("truffle-hdwallet-provider");
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const fs = require('fs');
 
-var infura_apikey = "";
-var mnemonic = "";
+let secrets;
 
+if (fs.existsSync('secrets.json')) {
+    secrets = JSON.parse(fs.readFileSync('secrets.json', 'utf8'));
+} else {
+    throw new Error('No secrets.json found!');
+}
+
+console.log(`Using mnemonic '${secrets.mnemonic}'.`);
+console.log(`Using infura api key '${secrets.infuraApiKey}'.`);
 
 module.exports = {
     networks: {
         development: {
-            host: "localhost",
+            host: 'localhost',
             port: 8545,
-            network_id: "*" // Match any network id
+            network_id: '*' // Match any network id
         },
         ropsten: {
-            provider: new HDWalletProvider(mnemonic, "https://ropsten.infura.io/" + infura_apikey),
+            provider: new HDWalletProvider(secrets.mnemonic, 'https://ropsten.infura.io/' + secrets.infuraApiKey),
             network_id: 3,
-            gas: 88000,
-            gasPrice: 20000000000 // 20 Gwei
+            gas: 250000,
+            gasPrice: 10000000000 // 10 Gwei
         },
         mainnet: {
-            provider: new HDWalletProvider(mnemonic, "https://mainnet.infura.io/" + infura_apikey),
+            provider: new HDWalletProvider(secrets.mnemonic, 'https://mainnet.infura.io/' + secrets.infuraApiKey),
             network_id: 1,
-            gas: 88000,
-            gasPrice: 20000000000 // 20 Gwei
+            gas: 250000,
+            gasPrice: 50000000000 // 50 Gwei
         }
-    }
+    },
+    secrets: secrets
 };
